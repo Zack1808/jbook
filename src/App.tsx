7109bb9b167a5509c0496b7ab212d9c9a09c30b2
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2024-01-09 12:54:24 +0100

    Added textarea and button. Setup controlled input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  const [input, setInput] = useState<string>("");
+  const [code, setCode] = useState<string>("");
+
+  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) =>
+    setInput(event.target.value);
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={onChange}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <div>{code}</div>
+    </div>
+  );
 };
 
 export default App;
