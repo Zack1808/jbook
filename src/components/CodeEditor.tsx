0f5b165069991c5ce908793f0c1b61461bfcab47
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2024-01-10 15:59:43 +0100

    Added configuration to the editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,7 +1,26 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-  return <MonacoEditor height="500px" />;
+  return (
+    <MonacoEditor
+      height="500px"
+      theme="vs-dark"
+      language="javascript"
+      options={{
+        wordWrap: "on",
+        minimap: {
+          enabled: false,
+        },
+        tabIndex: 2,
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
