d6e38c8c66f8260fe2f25bb2ed9d119d1d451022
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2024-01-11 11:45:02 +0100

    Fixed bug with resizing the browser window (editor and preview are now visible after browser resize)

--- src/components/Resizable.tsx
+++ src/components/Resizable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { ResizableBox } from "react-resizable";
 
 import "./Resizable.css";
@@ -8,19 +9,40 @@ interface ResizableType {
 }
 
 const Resizable: React.FC<ResizableType> = ({ axis, children }) => {
+  const [height, setHeight] = useState<number>(window.innerHeight);
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    let timer: any;
+
+    const listener = () => {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        setHeight(window.innerHeight);
+        setWidth(window.innerWidth);
+      }, 100);
+    };
+
+    window.addEventListener("resize", listener);
+
+    return () => {
+      window.removeEventListener("resize", listener);
+    };
+  }, []);
+
   return (
     <ResizableBox
       className={
         axis === "horizontal" ? "resize-horizontal" : "resize-vertical"
       }
       height={axis === "horizontal" ? Infinity : 300}
-      width={axis === "horizontal" ? window.innerWidth * 0.75 : Infinity}
+      width={axis === "horizontal" ? width * 0.75 : Infinity}
       axis={axis === "horizontal" ? "x" : "y"}
       resizeHandles={axis === "horizontal" ? ["e"] : ["s"]}
       maxConstraints={
         axis === "horizontal"
-          ? [window.innerWidth * 0.9, Infinity]
-          : [Infinity, window.innerHeight * 0.9]
+          ? [width * 0.9, Infinity]
+          : [Infinity, height * 0.9]
       }
     >
       {children}
