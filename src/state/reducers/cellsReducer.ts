6576192690a973fb1c45506631268a26e12f2b3e
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2024-01-12 20:22:41 +0100

    Fixed some last errors

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -20,45 +20,58 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = produce((state: CellsState = initialState, action: Action) => {
-  switch (action.type) {
-    case ActionType.UPDATE_CELL:
-      const { id, content } = action.payload;
-      state.data[id].content = content;
-      return;
+const reducer = produce(
+  (state: CellsState = initialState, action: Action): CellsState => {
+    switch (action.type) {
+      case ActionType.UPDATE_CELL:
+        const { id, content } = action.payload;
 
-    case ActionType.DELETE_CELL:
-      delete state.data[action.payload];
-      state.order = state.order.filter((id) => id !== action.payload);
-      return;
+        state.data[id].content = content;
 
-    case ActionType.MOVE_CELL:
-      const { direction } = action.payload;
-      const index = state.order.findIndex((id) => id === action.payload.id);
-      const targetIndex = direction === "up" ? index - 1 : index + 1;
-      if (targetIndex < 0 || targetIndex > state.order.length - 1) return;
-      state.order[index] = state.order[targetIndex];
-      state.order[targetIndex] = action.payload.id;
-      return;
+        return state;
 
-    case ActionType.INSERT_CELL_BEFORE:
-      const cell: Cell = {
-        content: "",
-        type: action.payload.type,
-        id: randomId(),
-      };
-      state.data[cell.id] = cell;
-      const insertingIndex = state.order.findIndex(
-        (id) => id === action.payload.id
-      );
-      if (insertingIndex < 0) state.order.push(cell.id);
-      else state.order.splice(insertingIndex, 0, cell.id);
-      return;
+      case ActionType.DELETE_CELL:
+        delete state.data[action.payload];
+        state.order = state.order.filter((id) => id !== action.payload);
 
-    default:
-      return state;
-  }
-});
+        return state;
+
+      case ActionType.MOVE_CELL:
+        const { direction } = action.payload;
+        const index = state.order.findIndex((id) => id === action.payload.id);
+        const targetIndex = direction === "up" ? index - 1 : index + 1;
+
+        if (targetIndex < 0 || targetIndex > state.order.length - 1)
+          return state;
+
+        state.order[index] = state.order[targetIndex];
+        state.order[targetIndex] = action.payload.id;
+
+        return state;
+
+      case ActionType.INSERT_CELL_BEFORE:
+        const cell: Cell = {
+          content: "",
+          type: action.payload.type,
+          id: randomId(),
+        };
+        const insertingIndex = state.order.findIndex(
+          (id) => id === action.payload.id
+        );
+
+        state.data[cell.id] = cell;
+
+        if (insertingIndex < 0) state.order.push(cell.id);
+        else state.order.splice(insertingIndex, 0, cell.id);
+
+        return state;
+
+      default:
+        return state;
+    }
+  },
+  initialState
+);
 
 const randomId = () => {
   return Math.random().toString(36).substring(2, 5);
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 
 import reducers from "./reducers";
 
-//@ts-ignore
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
